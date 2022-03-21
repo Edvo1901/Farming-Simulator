6465c5a9c0726be12016c1cc3cf0580c4db5fe34
dongvo191 <DongVo1901@gmail.com>
2022-03-21 16:16:42 +1030

    basic attributes and method for Item class

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,5 +1,58 @@
 package students.items;
 
 public class Item {
-
+	
+	private int age = 0;
+	private int maturationAge;
+	private int deathAge;
+	private int monteryValue;
+	
+	public Item(int age, int maturationAge, int deathAge, int monteryValue) {
+		this.age = 0;
+		this.maturationAge = maturationAge;
+		this.deathAge = deathAge;
+		this.monteryValue = monteryValue;
+	}
+	
+	public void tick() {
+		this.age++;
+	}
+	
+	public void setAge(int newAge) {
+		this.age = newAge;
+	}
+	
+	public boolean died() {
+		if (this.age > this.deathAge) {
+			return true;
+		}
+		return false;
+	}
+	
+	public int getValue() {
+		if (this.age > this.maturationAge) {
+			return this.monteryValue;
+		}
+		return 0;
+	}
+	
+	public boolean equals(Object compare) {
+		if (compare instanceof Item) {
+			Item otherItem = (Item) compare;
+			if (this.age == otherItem.age) {
+				if (this.deathAge == otherItem.deathAge) {
+					if (this.maturationAge == otherItem.maturationAge) {
+						if (this.monteryValue == otherItem.monteryValue) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	public abstract String toString() {
+		//Leave this for now
+	}
 }
