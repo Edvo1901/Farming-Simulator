4367ce12b780d3a485c197a7b4ce30f6ea1e6584
dongvo191 <DongVo1901@gmail.com>
2022-03-21 16:37:32 +1030

    Update Grain
    create a super method for Grain that inherited from Item and add a getGenerationCount method that return total number of grain object that have been instantiated

--- src/students/items/Grain.java
+++ src/students/items/Grain.java
@@ -1,6 +1,25 @@
 package students.items;
 
-public class Grain{
-
+public class Grain extends Food{
+	int grainObject = 0;
 	
+
+	public Grain(int age, int maturationAge, int deathAge, int monteryValue, String itemName) {
+		super(age, maturationAge, deathAge, monteryValue, itemName);
+		maturationAge = 2;
+		deathAge = 6;
+		monteryValue = 2;
+		
+		if (age < maturationAge) {
+			itemName = "g";
+		} else {
+			itemName = "G";
+		}
+		
+		grainObject++;
+	}
+	
+	public int getGenerationCount() {
+		return grainObject;
+	}
 }
