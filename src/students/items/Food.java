5dc240fd05218f05299b8291fa57a02f47fa076d
dongvo191 <DongVo1901@gmail.com>
2022-03-21 16:25:04 +1030

    make Food subclass of Item
    Make Food inherited from Item and add super class

--- src/students/items/Food.java
+++ src/students/items/Food.java
@@ -1,6 +1,8 @@
 package students.items;
 
-public class Food {
+public class Food extends Item {
 
-
+	public Food(int age, int maturationAge, int deathAge, int monteryValue) {
+		super(age, maturationAge, deathAge, monteryValue);
+	}
 }
