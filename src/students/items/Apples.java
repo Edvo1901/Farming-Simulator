f7e655e0cfb9d30bd266949dca4c2ca7e8f56628
dongvo191 <DongVo1901@gmail.com>
2022-03-21 16:44:35 +1030

    Update Apple class
    make a super method for Apple class that inherited from Item class and add a getGenerationCount, which return number of time that apple object have been instantiated

--- src/students/items/Apples.java
+++ src/students/items/Apples.java
@@ -1,5 +1,24 @@
 package students.items;
 
-public class Apples {
+public class Apples extends Item{
+	int appleObject = 0;
 
+	public Apples(int age, int maturationAge, int deathAge, int monteryValue, String itemName) {
+		super(age, maturationAge, deathAge, monteryValue, itemName);
+		maturationAge = 3;
+		deathAge = 5;
+		monteryValue = 3;
+		
+		if (age < maturationAge) {
+			itemName = "a";
+		} else {
+			itemName = "A";
+		}
+		
+		appleObject++;
+	}
+	
+	public int getGenerationCount() {
+		return appleObject;
+	}
 }
