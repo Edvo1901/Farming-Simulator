77cad92eb408cca06add8a4ee023351b713a80b0
dongvo191 <DongVo1901@gmail.com>
2022-03-22 11:12:38 +1030

    add super class
    add super class for Weed class and assign value for variable

--- src/students/items/Weed.java
+++ src/students/items/Weed.java
@@ -1,6 +1,20 @@
 package students.items;
 
-public class Weed
-{
+public class Weed extends Item {
+	//Declare variable of the class
+	private double age = 0;
+	private double maturationAge;
+	private double deathAge;
+	private double monteryValue;
+	private String itemName = " ";
+
+	public Weed(double maturationAge, double deathAge, double monteryValue) {
+		super(maturationAge, deathAge, monteryValue);
+		maturationAge = Double.POSITIVE_INFINITY;
+		deathAge = Double.POSITIVE_INFINITY;
+		monteryValue = -1;
+		
+		itemName = "#";
+	}
 
 }
