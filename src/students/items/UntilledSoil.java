53067a95578944ba9d90138163e45bb029d15ef3
dongvo191 <DongVo1901@gmail.com>
2022-03-22 11:14:01 +1030

    add super method
    add super method for UntilledSoil class and assign value to variable

--- src/students/items/UntilledSoil.java
+++ src/students/items/UntilledSoil.java
@@ -1,6 +1,21 @@
 package students.items;
 
-public class UntilledSoil {
+public class UntilledSoil extends Item {
+	//Declare variable of the class
+	private double age = 0;
+	private double maturationAge;
+	private double deathAge;
+	private double monteryValue;
+	private String itemName = " ";
+
+	public UntilledSoil(double maturationAge, double deathAge, double monteryValue) {
+		super(maturationAge, deathAge, monteryValue);
+		maturationAge = Double.POSITIVE_INFINITY;
+		deathAge = Double.POSITIVE_INFINITY;
+		monteryValue = -1;
+		
+		itemName = "/";
+	}
 
 
 }
